1e8a3189d91e648a2addb2d971eabb654b409eb6
Shreya1103 <90048107+Shreya1103@users.noreply.github.com>
2023-02-18 23:19:47 +0530

    Add files via upload

---
+++ internalCode/RestHouse.java
@@ -0,0 +1,11 @@
+package internalCode;
+
+public class RestHouse extends Box{
+	public RestHouse(int pos) {
+		setPosition(pos);
+		setName("Rest House");
+		setBuyable(false);
+		setColourGroup("None");
+	}
+
+}
